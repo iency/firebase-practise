af25f5e87733c8ed684c75f75fa7ab24466596c0
iency <pwnzjake@gmail.com>
2022-07-17 18:16:55 +0300

    123

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCajDE85M0K48k63U8sI7h5d2Sn6EWOB-w",
+  authDomain: "fir-practise-5a62d.firebaseapp.com",
+  projectId: "fir-practise-5a62d",
+  storageBucket: "fir-practise-5a62d.appspot.com",
+  messagingSenderId: "356729804751",
+  appId: "1:356729804751:web:ea8d1fdc63725f76a624ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
