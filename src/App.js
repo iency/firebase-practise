186587844416ddad759a899ac71a8391ea8704eb
iency <pwnzjake@gmail.com>
2022-07-21 21:39:24 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
-import { auth } from './firebase/init';
+import { auth, db } from './firebase/init';
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
 
 function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+
+  function createPost() {
+   const post = {
+    title: "Land a $100k job",
+    description: "Finish Frontend Simplified",
+   };
+   addDoc(collection(db, "posts"), post)
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    const posts = docs.map(elem => elem.data());
+    console.log(posts)
+  }
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -49,6 +64,8 @@ function App() {
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
       {loading ? 'loading...' : user.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
     </div>
   );
 }
